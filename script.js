e55be256478c0aa5ba96329394205508766accc4
mohammed yaseen <mhdyaseen.official@gmail.com>
2021-06-21 23:21:43 +0530

    add some script

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function buttonclick(num){
     console.log("wrk"+num)
-   document.getElementById("screen").value+=num ;
+   document.getElementById("screen").value+=num 
 } 
+function clrscr(){
+    document.getElementById("screen").value=""
+}
